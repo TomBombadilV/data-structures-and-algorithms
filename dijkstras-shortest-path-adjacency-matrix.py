c51aa0965d62fec3d82b03dcf89d7fb0fd93f149
Carolyn <carolyngychan@gmail.com>
2020-04-13 18:34:43 -0700

    Dijkstras with adjacency matrix

--- dijkstras-shortest-path-adjacency-matrix.py
+++ dijkstras-shortest-path-adjacency-matrix.py
@@ -1,8 +1,14 @@
 # Dijkstra's Shortest Path implementation with adjacency matrix
+# Two methods:
+# 1. Naive method with exhaustive search => O(V * E)
+# 2. Heap method with min heap => O(VlogE)
+# This implementation uses method #1
 
 from graph import WeightedAdjacencyMatrix
 from typing import Dict, List
 from queue import PriorityQueue
 
 def dijkstras(g: WeightedAdjacencyMatrix, s: int, t: int) -> int:
-    pass
+    explored = [False] * g.size()
+    dist = [None] * g.size()
+
