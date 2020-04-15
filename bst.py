6961f7be2c37aca99ffced90ea8c98c3076b8ec3
Carolyn <carolyngychan@gmail.com>
2020-04-15 16:07:35 -0700

    Diameter method

--- bst.py
+++ bst.py
@@ -1,4 +1,4 @@
-from random import Shuffle
+from random import shuffle
 
 class BSTNode:
     def __init__(self, val: int):
@@ -9,7 +9,7 @@ class BSTNode:
 class BinarySearchTree:
     def __init__(self):
          self.root = None
-
+    
     def insert(self, val) -> None:
         if not(self.root):
             self.root = BSTNode(val)
@@ -31,13 +31,52 @@ class BinarySearchTree:
             # No duplicates plz 
             else:
                 return
+    
+    def diameter(self) -> int:
+        def diameter_left(node: BSTNode, left_width: int) -> int:
+            if not(node):
+                return 0
+            left_width += max(  diameter_left(node.left, 1), \
+                                diameter_left(node.right, -1))
+            return left_width
+            
+        def diameter_right(node: BSTNode, right_width: int) -> int:
+            if not(node):
+                return 0
+            right_width += max( diameter_right(node.left, -1), \
+                                diameter_right(node.right, 1))
+            return right_width
+
+        if not(self.root): return 0
+        return 1 \
+               + diameter_left(self.root.left, 1)\
+               + diameter_right(self.root.right, 1)
+
+    def print_tree(self) -> None:
+        q = []
+        curr = self.root
+        level = 0
+        while curr or q:
+            print(curr.val, end=" ")
+            if curr.left:
+                q.append((curr.left, level + 1))
+            if curr.right:
+                q.append((curr.right, level + 1))
+            prev_level = level
+            curr, level = q[0] if q else (None, None)
+            if not(level == prev_level):
+                print()
+            q = q[1:]
+
 
 class RandomBST(BinarySearchTree):
     def __init__(self, n: int):
+        super().__init__()
         l = [i for i in range(n)]
+        shuffle(l)
+        for i in l:
+            self.insert(i)
 
-
-
-
-bst = BinarySearchTree()
-
+rbst = RandomBST(10)
+rbst.print_tree()
+print("Width: ", rbst.diameter())
