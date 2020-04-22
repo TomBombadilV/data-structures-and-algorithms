eecb858671f43a5707d12ab8264b40148bd37a6d
Carolyn <carolyngychan@gmail.com>
2020-04-21 18:21:47 -0700

    Renamed bst to binarysearchtree, avl tree implementation

---
+++ avltree.py
@@ -0,0 +1,31 @@
+from binarysearchtree import BSTNode, BinarySearchTree
+
+class AVLNode(BSTNode):
+    def __init__(self, val: int):
+        super().init__(val)
+        # Difference between left and right subtree heights
+        # Height of right - height of left
+        self.balance = 0
+
+class AVLTree(BinarySearchTree):
+    def __init__(self):
+        super().__init__()
+
+    def insert(self, val: int) -> None:
+        def recurse(root: AVLNode, val: int)
+            if not(self.root):
+                self.root = AVLNode(val)
+            if val < root.val:
+                root.balance -= 1
+                if root.left:
+                    recurse(root.left, val)
+                else:
+                    root.left = AVLNode(val)
+            elif val > root.val:
+                root.balance += 1
+                if root.right:
+                    recurse(root.right, val)
+                else:
+                    root.right = AVLNode(val)
+            else:
+                return
